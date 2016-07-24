4b1d098c4b46440cde7f7b355037c85553161c30
Omar Faruqe <faruqe.cse@gmail.com>
2016-07-24 17:17:08 -0400

    Getting all messages from MessageDatabase Class

---
+++ src/main/java/com/mc/messenger/model/Profile.java
@@ -0,0 +1,57 @@
+package com.mc.messenger.model;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement
+public class Profile {
+
+	private long id;
+	private Date created;
+	private String profileName, firstName, lastName;
+	
+	
+	public Profile() {
+	}
+
+	public Profile(long id, String profileName, String firstName, String lastName) {
+		this.id = id;
+		this.profileName = profileName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	public String getProfileName() {
+		return profileName;
+	}
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+}
