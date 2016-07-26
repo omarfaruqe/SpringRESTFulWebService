6ae26c82b99ec1fdd68f07476afd1cab2e8a3f27
Omar Faruqe <faruqe.cse@gmail.com>
2016-07-25 20:44:33 -0400

    Filtering and Pagination Implemented

--- src/main/java/com/mc/messenger/resources/MessageResource.java
+++ src/main/java/com/mc/messenger/resources/MessageResource.java
@@ -9,98 +9,60 @@ import com.mc.messenger.model.Message;
 import com.mc.messenger.service.MessageService;
 
 @Path("/messages")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class MessageResource {
-	
-//	@GET
-//	@Produces(MediaType.TEXT_PLAIN)
-//	public String getMessages(){
-//		return "Hello World!";
-//	}
-	
+
 	MessageService messageService = new MessageService();
-	
 
-	
-	//Retrieving all data in JSON format
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Message> getMessagesJSON(){
-		return messageService.getAllMessages();
-	}
-
-	
-	//Retrieving data by messageId in JSON format
-	@GET
-	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message getMessageByIdJSON(@PathParam("messageId") long messageId){
-		return  messageService.getMessage(messageId);
-	}
-		
-	//Insert or add test (Need to specify body as message and content type)
-	@POST
-//	@Path("/addMessage")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message addMessageJSON(Message message){
-		return messageService.addMessage(message);
-	}
-	
-	//Update message using messageId in JSON format
-	@PUT
-	@Path("/{messageId}")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message updateMessageJSON(@PathParam("messageId") long messageId, Message message){
-		//message.setId(messageId);
-		return messageService.addMessage(message);
-	}
-		
-	//Delete message by messageId in JSON format
-	@DELETE
-	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message deleteMessageJSON(@PathParam("messageId") long messageId){
-		return messageService.removeMessage(messageId);
-	}
-	
-	
-	//****************** XML Format **************************************//
-//	
-//	//Retrieving all data in XML format
+//	// Retrieving all data in JSON format
 //	@GET
-//	@Path("/allMessageXML")
-//	@Produces(MediaType.APPLICATION_XML)
-//	public List<Message> getMessagesXML(){
+//	public List<Message> getMessagesJSON() {
 //		return messageService.getAllMessages();
 //	}
-//	
-//	//Retrieving data by messageId in XML format
-//	@GET
-//	@Path("/xml/{messageId}")
-//	@Produces(MediaType.APPLICATION_XML)
-//	public Message getMessageByIdXML(@PathParam("messageId") long messageId){
-//		return  messageService.getMessage(messageId);
-//	}
-//	
-//	
-//	//Update message using messageId in XML format
-//	@PUT
-//	@Path("/updateMessageXML/{messageId}")
-//	@Consumes(MediaType.APPLICATION_XML)
-//	@Produces(MediaType.APPLICATION_XML)
-//	public Message updateMessageXML(@PathParam("messageId") long messageId, Message message){
-//		message.setId(messageId);
-//		return messageService.addMessage(message);
-//	}
-//	
-//	//Delete message by messageId in XML format
-//	@DELETE
-//	@Path("deleteMessageXML/{messageId}")
-//	@Produces(MediaType.APPLICATION_XML)
-//	public Message deleteMessageXML(@PathParam("messageId") long messageId){
-//		return messageService.removeMessage(messageId);
-//	}
 	
+	// Retrieving all data in JSON format (accepting parameter)
+	@GET
+	public List<Message> getMessagesJSON(@QueryParam("year") int year,
+										@QueryParam("start") int start,
+										@QueryParam("size") int size) {
+		if(year>0){
+			return messageService.getAllMessagesForYear(year);
+		}
+		if(start >= 0 && size >= 0){
+			return messageService.getAllMessagesPaginated(start, size);
+		}
+		else{
+			return messageService.getAllMessages();
+		}		
+	}
 	
+
+	// Retrieving data by messageId in JSON format
+	@GET
+	@Path("/{messageId}")
+	public Message getMessageByIdJSON(@PathParam("messageId") long messageId) {
+		return messageService.getMessage(messageId);
+	}
+
+	// Insert or add test (Need to specify body as message and content type)
+	@POST
+	public Message addMessageJSON(Message message) {
+		return messageService.addMessage(message);
+	}
+
+	// Update message using messageId in JSON format
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessageJSON(@PathParam("messageId") long messageId, Message message) {
+		message.setId(messageId);
+		return messageService.addMessage(message);
+	}
+
+	// Delete message by messageId in JSON format
+	@DELETE
+	@Path("/{messageId}")
+	public Message deleteMessageJSON(@PathParam("messageId") long messageId) {
+		return messageService.removeMessage(messageId);
+	}
 }
--- src/main/java/com/mc/messenger/service/MessageService.java
+++ src/main/java/com/mc/messenger/service/MessageService.java
@@ -8,29 +8,51 @@ import com.mc.messenger.model.Message;
 public class MessageService {
 
 	private Map<Long, Message> messagesDatabase = DatabaseClass.getMessages();
-	
-	public MessageService(){
+
+	public MessageService() {
 		messagesDatabase.put(1L, new Message(1L, "Montreal", "Omar"));
 		messagesDatabase.put(2L, new Message(2L, "College", "Faruqe"));
 		messagesDatabase.put(3L, new Message(3L, "Technology", "Omar"));
+		messagesDatabase.put(4L, new Message(4L, "Engineering", "Faruqe"));
 	}
-	
-	//Retrieving all messages
-	public List<Message>getAllMessages(){
+
+	// Retrieving all messages
+	public List<Message> getAllMessages() {
 		return new ArrayList<Message>(messagesDatabase.values());
 	}
-	
+
+	// Retrieving all messages in a specific year
+	public List<Message> getAllMessagesForYear(int year) {
+		List<Message> messagesForYear = new ArrayList<>();
+		Calendar cal = Calendar.getInstance();
+		for (Message message : messagesDatabase.values()) {
+			cal.setTime(message.getCreated());
+			if (cal.get(Calendar.YEAR) == year) {
+				messagesForYear.add(message);
+			}
+		}
+		return messagesForYear;
+	}
+
+	// Retrieving all messages in paginated
+	public List<Message> getAllMessagesPaginated(int start, int size) {
+		ArrayList<Message> list = new ArrayList<Message>(messagesDatabase.values());
+		if (start + size > list.size())
+			return new ArrayList<Message>();
+		return list.subList(start, start + size);
+	}
+
 	// Retrieving specific message by its id
-	public Message getMessage(long id){
+	public Message getMessage(long id) {
 		return messagesDatabase.get(id);
 	}
-	
-	public Message addMessage(Message message){
-		message.setId(messagesDatabase.size()+1);
+
+	public Message addMessage(Message message) {
+		message.setId(messagesDatabase.size() + 1);
 		messagesDatabase.put(message.getId(), message);
 		return message;
 	}
-	
+
 	public Message updateMessage(Message message) {
 		if (message.getId() <= 0) {
 			return null;
@@ -38,8 +60,8 @@ public class MessageService {
 		messagesDatabase.put(message.getId(), message);
 		return message;
 	}
-	
-	public Message removeMessage(long id){
+
+	public Message removeMessage(long id) {
 		return messagesDatabase.remove(id);
-	}	
+	}
 }
